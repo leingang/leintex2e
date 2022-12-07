e36c0e5ba8b708a80810184eb552042c1c8ba736
Matthew Leingang <leingang@nyu.edu>
2022-12-07 10:15:12 -0500

    Encapsulate into l3build module

---
+++ build.lua
@@ -0,0 +1,9 @@
+#!/usr/bin/env texlua
+
+module = "leintex2e"
+
+-- This ancient package has never been packaged into a docstrip file.
+-- Nor is there any documentation as of yet.
+-- So the only thing l3build needs to do is install it!
+sourcefiles = {"leintex2e.sty"}
+
